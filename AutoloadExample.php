17f3db23d565e7b911c39a56ddf5d58d58fc3e82
sai.kuncha <sai.kuncha@raybiztech.com>
2017-08-31 14:43:12 +0530

    test

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace sms\smscom;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
